31600fc87df59b9a5ee06f4732312a27f990560c
Daniilllllll <privetdanilka22@gmail.com>
2024-09-16 17:12:14 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print((9**0.5)*5)
+
+#2hd program
+print((9.98<9.99>1000)!=1000.1)
+
+#3rd program
+print((2*2+2)==(2*(2+2)))
+
+#4th program
+print(int(10*(((int((10*(float('123.456')))//1.0))/10)%123)))
