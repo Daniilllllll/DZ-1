a5793347c1e6c6f47e3a18e73dad63ae28e1fc1a
Daniilllllll <privetdanilka22@gmail.com>
2024-09-16 19:14:04 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 print((9**0.5)*5)
 
 #2hd program
-print((9.98<9.99>1000)!=1000.1)
+print(9.99>9.98 and 1000!= 1000.1)
 
 #3rd program
 print((2*2+2)==(2*(2+2)))
 
 #4th program
-print(int(10*(((int((10*(float('123.456')))//1.0))/10)%123)))
+print((int((float('123.456'))*10))%10)
